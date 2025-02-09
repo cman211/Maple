abdecfbcd9207fa663570fd9f4cc3c0144965c60
cman211 <cormacfleming1@gmail.com>
2025-02-09 16:20:56 +0000

    Update getServerCount.js

--- netlify/functions/getServerCount.js
+++ netlify/functions/getServerCount.js
@@ -2,22 +2,19 @@ const fetch = require("node-fetch");
 
 exports.handler = async function () {
     try {
-        // Fetch server count from Discord API
         const response = await fetch("https://discord.com/api/v10/users/@me/guilds", {
             method: "GET",
             headers: {
-                "Authorization": `Bot ${process.env.BOT_TOKEN}`, // Use bot token from environment variable
+                "Authorization": `Bot ${process.env.BOT_TOKEN}`,
             },
         });
 
-        // If the response is not OK, throw an error
         if (!response.ok) {
             throw new Error(`Discord API returned ${response.status}`);
         }
 
         const data = await response.json();
 
-        // Return server count
         return {
             statusCode: 200,
             body: JSON.stringify({ server_count: data.length }),
