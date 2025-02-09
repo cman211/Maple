e34efced1e25a74e620b08b97c74d68a2ac05e3a
cman211 <cormacfleming1@gmail.com>
2025-02-09 17:24:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     try {
         const response = await fetch("/.netlify/functions/getServerCount");
+
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
