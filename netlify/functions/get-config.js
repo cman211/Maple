e7fb04e2f4d6b366b7cd18603b52d0e90431814a
cman211 <cormacfleming1@gmail.com>
2025-02-09 20:52:30 +0000

    Update get-config.js

--- netlify/functions/get-config.js
+++ netlify/functions/get-config.js
@@ -2,7 +2,7 @@ exports.handler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      clientId: process.env.DISCORD_CLIENT_ID, // Get from Netlify env vars
+      clientId: process.env.DISCORD_CLIENT_ID, // Securely fetch from Netlify env vars
     }),
   };
 };
